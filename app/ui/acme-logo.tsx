d95d7a31973579b6964b06d64939ab44938b0c0d
lcz <1543812491@qq.com>
2023-11-02 19:55:16 +0800

    add ui fonts file

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { Inter } from 'next/font/google';
+import { inter } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${Inter.className} flex flex-row items-center leading-none text-white`}
+      className={`${inter.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { clsx } from 'clsx';
 import Link from 'next/link';
-import { Inter } from 'next/font/google'
+import { inter } from '@/app/ui/fonts';
 
 interface Breadcrumb {
   label: string;
@@ -15,7 +15,7 @@ export default function Breadcrumbs({
 }) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(Inter.className, 'flex text-xl md:text-2xl')}>
+      <ol className={clsx(inter.className, 'flex text-xl md:text-2xl')}>
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
