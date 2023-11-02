7c648856708dbefe82e8dd78bf194792704b381b
lcz <1543812491@qq.com>
2023-11-02 20:46:05 +0800

    add global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import './ui/global.css'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className='bg-black'>
+      <body className={inter.className} style={{ background: 'transparent', height: '100vh', margin: 0 }}>
+      <Link href="/" style={{ position: 'fixed' }} className='text-white fixed top-5 left-5 z-50'>Back to Home</Link>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
